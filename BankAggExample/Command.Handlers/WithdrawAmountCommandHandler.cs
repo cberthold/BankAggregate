0562ba00f78265a18ef03359fc8199f4ac111578
Chris Berthold <cberthold@baxtertechnology.com>
2018-02-21 21:36:39 -0500

    build out basic command handler structures

---
+++ BankAggExample/Command.Handlers/WithdrawAmountCommandHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankAggExample.Command.Handlers
+{
+    public class WithdrawAmountCommandHandler
+    {
+    }
+}
---
+++ BankAggExample/Command/TransferFundsCommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankAggExample.Command
+{
+    public class TransferFundsCommand
+    {
+    }
+}
